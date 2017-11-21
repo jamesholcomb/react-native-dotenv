7da27fe49a6b2e507481aeb3f1adce8871ac7ae1
James Holcomb <james.holcomb@gmail.com>
2017-11-21 16:25:57 -0600

    Revert scope, add semi

--- babel-plugin-dotenv/index.js
+++ babel-plugin-dotenv/index.js
@@ -1,57 +1,70 @@
-const dotEnv = require("dotenv")
-const sysPath = require("path")
-const process = require("process")
+const dotEnv = require("dotenv");
+const sysPath = require("path");
+const process = require("process");
 
 module.exports = function(data) {
-  const t = data.types
+  const t = data.types;
 
   return {
     visitor: {
       ImportDeclaration: function(path, state) {
-        const options = state.opts
+        const options = state.opts;
 
-        if (options.replacedModuleName === undefined) return
+        if (options.replacedModuleName === undefined) return;
 
-        const configDir = options.configDir ? options.configDir : "./"
-        const configFile = options.filename ? options.filename : ".env"
-        const env = process.env.BABEL_ENV || "development"
+        const configDir = options.configDir ? options.configDir : "./";
+        const configFile = options.filename ? options.filename : ".env";
+        const env = process.env.BABEL_ENV || "development";
 
         if (path.node.source.value === options.replacedModuleName) {
-          const config = dotEnv.config({
-            path: sysPath.join(configDir, `${configFile}`),
-            silent: true
-          }) || {}
+          const config =
+            dotEnv.config({
+              path: sysPath.join(configDir, `${configFile}`),
+              silent: true
+            }) || {};
 
-          const envConfig = Object.assign({}, config, dotEnv.config({
-            path: sysPath.join(configDir, `${configFile}.${env}`),
-            silent: true
-          }))
+          const envConfig = Object.assign(
+            {},
+            config,
+            dotEnv.config({
+              path: sysPath.join(configDir, `${configFile}.${env}`),
+              silent: true
+            })
+          );
 
-          const iOSConfig = Object.assign({}, envConfig, dotEnv.config({
-            path: sysPath.join(configDir, `${configFile}.${env}.ios`),
-            silent: true
-          }))
+          const iOSConfig = Object.assign(
+            {},
+            envConfig,
+            dotEnv.config({
+              path: sysPath.join(configDir, `${configFile}.${env}.ios`),
+              silent: true
+            })
+          );
 
-          const androidConfig = Object.assign({}, envConfig, dotEnv.config({
-            path: sysPath.join(configDir, `${configFile}.${env}.android`),
-            silent: true
-          }))
+          const androidConfig = Object.assign(
+            {},
+            envConfig,
+            dotEnv.config({
+              path: sysPath.join(configDir, `${configFile}.${env}.android`),
+              silent: true
+            })
+          );
 
           path.node.specifiers.forEach(function(specifier, idx) {
             if (specifier.type === "ImportDefaultSpecifier") {
               throw path
                 .get("specifiers")
-                [idx].buildCodeFrameError("Default imports are not supported")
+                [idx].buildCodeFrameError("Default imports are not supported");
             }
-            const importedId = specifier.imported.name
-            const localId = specifier.local.name
+            const importedId = specifier.imported.name;
+            const localId = specifier.local.name;
 
             if (!iOSConfig[importedId]) {
               throw path
                 .get("specifiers")
                 [idx].buildCodeFrameError(
                   `Import variable ${importedId} not found in any .env files`
-                )
+                );
             }
 
             if (!androidConfig[importedId]) {
@@ -59,23 +72,23 @@ module.exports = function(data) {
                 .get("specifiers")
                 [idx].buildCodeFrameError(
                   `Import variable ${importedId} not found`
-                )
-            }            
+                );
+            }
 
-            const binding = path.scope.getBinding(localId)
+            const binding = path.scope.getBinding(localId);
             binding.referencePaths.forEach(function(refPath) {
               if (iOSConfig[importedId] && androidConfig[importedId]) {
                 const val = `require("Platform").OS === "ios" ? "${iOSConfig[
                   importedId
-                ]}" : "${androidConfig[importedId]}"`
-                refPath.replaceWithSourceString(val)
+                ]}" : "${androidConfig[importedId]}"`;
+                refPath.replaceWithSourceString(val);
               }
-            })
-          })
+            });
+          });
 
-          path.remove()
+          path.remove();
         }
       }
     }
-  }
-}
+  };
+};
